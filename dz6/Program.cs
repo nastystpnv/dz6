b8a8c108685ca570af3db9aae74fabadd041a842
user <user@STAA>
2023-10-19 23:58:12 +0300

    Добавьте файлы проекта.

---
+++ dz6/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    public abstract class LocaL_resident
+    {
+        bool satiety;
+        public abstract void eat_garbage();
+        public abstract void eat_something();
+    }
+    public class Homeless : LocaL_resident
+    {
+        int count_clothes;
+        bool drunk;
+        bool satiety;
+        string name;
+        public Homeless( int count_clothes1, bool drunk1, bool satiety1, string name1)
+        {
+            count_clothes = count_clothes1;
+            drunk = drunk1;
+            satiety = satiety1;
+            name = name1;
+        }
+        public Homeless() { }
+        public override void eat_garbage () {
+            if (satiety)
+            {
+                Console.WriteLine("съел мусор");
+            }
+            else
+                Console.WriteLine("я сыт");
+        }
+        public override void eat_something()
+        {
+            if (drunk)
+            {
+                Console.WriteLine("съел кошку, проблевался");
+            }
+            else
+                Console.WriteLine("съел кошку, вкусно");
+        }
+
+    }
+    public class Animals : LocaL_resident
+    {
+        bool satiety;
+        bool predator;
+        bool isCat;
+        bool lichen;
+        public Animals(bool satiety1, bool predator1, bool cat_dog1, bool lichen1)
+        {
+            satiety = satiety1;
+            predator = predator1;
+            isCat = cat_dog1;
+            lichen = lichen1;
+        }
+        public Animals() { }
+        public override void eat_garbage()
+        {
+            if (!satiety)
+            {
+                Console.WriteLine("съел мусор");
+            }
+            else
+                Console.WriteLine("я сыт");
+        }
+        public override void eat_something()
+        {
+            if (predator)
+            {
+                Console.WriteLine("съел человека");
+            }
+            else
+                Console.WriteLine("съел мусор");
+        }
+        public void catordog()
+        {
+            if (isCat)
+            {
+                Console.WriteLine("мяу");
+            }
+            else
+                Console.WriteLine("непонятные звуки");
+
+        }
+    }
+    
+    public class Dumper
+    {
+        bool workind_day;
+        bool satiety;
+        bool trash;
+        string name;
+        public Dumper (bool working_day1, bool satiety1, bool trash1, string name1)
+        {
+            workind_day = working_day1;
+            satiety = satiety1;
+            trash = trash1;
+            name = name1;
+        }
+        
+        public void working()
+        {
+            if (workind_day)
+            {
+                Console.WriteLine("еду");
+            }
+            else
+                Console.WriteLine("отвалите");
+        }
+        public void satietys()
+        {
+            if (satiety)
+            {
+                Console.WriteLine("я сыт");
+            }
+            else
+                Console.WriteLine("я не буду есть мусор, поем дома");
+
+        }
+        
+
+    }
+    public class Junkyard
+    {
+        int trash;
+        string location;
+        bool inTown;
+        string name;
+        public Junkyard (int trash1, string location1, bool inTown1, string name1)
+        {
+            trash = trash1;
+            location = location1;
+            inTown = inTown1;
+            name = name1;
+        }
+        public void clearJunkyard()
+        {
+            trash = 0;
+            Console.WriteLine("мусора нет");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Homeless valera = new Homeless(2, true, true, "valera");
+            valera.eat_garbage();
+            valera.eat_something();
+            Animals maxic = new Animals (true, true, false, true);
+            maxic.eat_garbage();
+            maxic.eat_something();
+            maxic.catordog();
+            Dumper oleg = new Dumper(false, false, true, "oleg");
+            oleg.working();
+            oleg.satietys();
+            Junkyard heap = new Junkyard (50, "australia", false, "havana_club");
+            heap.clearJunkyard();
+            Console.ReadKey();
+
+        }
+
+    }
+    
+}
---
+++ tumakov7/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using System;
+
+namespace tumakov7
+{
+    class Program
+    {
+        enum BankAccountType
+        {
+            Current,
+            Saving
+        }
+        class BankAccount
+        {
+            private int account_number;
+            private double balance;
+            private BankAccountType type;
+            public int Number
+            {
+                get => account_number;
+                set => account_number = value;
+            }
+            public double Balance
+            {
+                get => balance;
+                set => balance = value;
+            }
+            public BankAccountType Type
+            {
+                get => type;
+                set => type = value;
+            }
+            static Random r = new Random();
+            private static int generic_number = r.Next(999, 10000);
+            public int Generic
+            {
+                get => generic_number;
+                set => generic_number++;
+            }
+            public double PutOnAccount(double balance)
+            {
+                Console.Write("Введите сумму для пополнения ");
+                double temp = Convert.ToDouble(Console.ReadLine());
+                return balance + temp;
+            }
+            public double WithdrawFromAccount(double balance)
+            {
+                Console.Write("Введите сумму для снятия ");
+                double temp = Convert.ToDouble(Console.ReadLine());
+                if (balance >= temp)
+                { return balance - temp; }
+                else
+                { Console.WriteLine("На вашем счёте недостаточно средств!"); return balance; }
+            }
+        }
+        class Building
+        {
+            private int number;
+            public int Number
+            {
+                get => number;
+                set => number = value;
+            }
+            private ushort height;
+            public ushort Height
+            {
+                get => height;
+                set => height = value;
+            }
+            private byte number_of_floors;
+            public byte Floors
+            {
+                get => number_of_floors;
+                set => number_of_floors = value;
+            }
+            private ushort number_of_flats;
+            public ushort Flats
+            {
+                get => number_of_flats;
+                set => number_of_flats = value;
+            }
+            private byte number_of_entrances;
+            public byte Entrance
+            {
+                get => number_of_entrances;
+                set => number_of_entrances = value;
+            }
+            static Random r = new Random();
+            public static int generic_number = r.Next(1000);
+            public Building(int n, ushort h, byte floors, ushort flats, byte entrances)
+            {
+                this.number = n;
+                this.height = h;
+                this.number_of_floors = floors;
+                this.number_of_flats = flats;
+                this.number_of_entrances = entrances;
+            }
+            public int FloorHeight(ushort h, byte f)
+            {
+                int temp = h / f;
+                return temp;
+            }
+            public int FlatsInEntrance(ushort f, byte e)
+            {
+                int temp = f / e;
+                return temp;
+            }
+            public int FlatsOnFloor(ushort flats, byte e, byte floors)
+            {
+                int temp = (flats / e) / (floors - 1);
+                return temp;
+            }
+        }
+        public static void Task_7_1()
+        {
+            BankAccount acc1 = new BankAccount();
+            acc1.Number = 1000;
+            acc1.Balance = 2300;
+            acc1.Type = BankAccountType.Current;
+            Console.WriteLine("Bank acccount №" + acc1.Number + " balance-" + acc1.Balance + "$ type-" + acc1.Type);
+        }
+        public static void Task_7_2()
+        {
+            BankAccount acc2 = new BankAccount();
+            acc2.Balance = 3000;
+            acc2.Type = BankAccountType.Saving;
+            Console.WriteLine("Bank acccount №" + acc2.Generic + " balance-" + acc2.Balance + "$ type-" + acc2.Type);
+        }
+        public static void Task_7_3()
+        {
+            BankAccount acc3 = new BankAccount();
+            acc3.Balance = 4000;
+            acc3.Type = BankAccountType.Current;
+            acc3.Balance = acc3.PutOnAccount(acc3.Balance);
+            Console.WriteLine("Bank acccount №" + acc3.Generic + " balance-" + acc3.Balance + "$ type-" + acc3.Type);
+            acc3.Balance = acc3.WithdrawFromAccount(acc3.Balance);
+            Console.WriteLine("Bank acccount №" + acc3.Generic + " balance-" + acc3.Balance + "$ type-" + acc3.Type);
+        }
+        public static void HomeTask_7_1()
+        {
+            Building building = new Building(1, 50, 16, 300, 5);
+            Console.WriteLine($"Высота одного этажа {building.FloorHeight(building.Height, building.Floors)} метра, количество квартир в подъезде {building.FlatsInEntrance(building.Flats, building.Entrance)}, количество квартир на этаже {building.FlatsOnFloor(building.Flats, building.Entrance, building.Floors)}");
+        }
+        static void Main(string[] args)
+        {
+            HomeTask_7_1();
+                Console.ReadKey();
+        }
+    }
+}
